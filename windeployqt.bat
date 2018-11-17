ef9ae88e12e12870d2eae66d05c8bba3b0ed5025
RunnanZhang <zhangqwerf@163.com>
2018-11-17 16:32:51 +0800

    增加发布程序的批处理

---
+++ windeployqt.bat
@@ -0,0 +1,4 @@
+call C:/Qt/Qt5.7.0/5.7/msvc2015_64/bin/qtenv2.bat
+call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat"
+windeployqt.exe --qmldir C:/Qt/Qt5.7.0/5.7/msvc2015_64/qml E:/MagicAssistant_bin/MagicAssistant.exe
+pause
